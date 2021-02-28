b585e2ece6a346644dc308d417a48bef4394c0d5
Harold <harold@pwrdrvr.com>
2021-02-27 22:49:46 -0500

    Get list of apps on server-side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,53 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { ApplicationClient, ApplicationResponse } from '../clients/DeployerClient';
+import fetch from 'node-fetch';
 
-export default function Home() {
+interface IPageProps {
+  props: {
+    apps: ApplicationResponse[];
+  };
+}
+
+export default function Home(context): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Release Console</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Release Console</h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <section>
+          <h2>Applications</h2>
+          <ul>
+            {context.props?.apps.map(({ appName, displayName }) => (
+              <li key={appName}>
+                <a>{displayName}</a>
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
+
+// This gets called on every request
+Home.getInitialProps = async function (): Promise<IPageProps> {
+  try {
+    const client = new ApplicationClient(undefined, { fetch });
+
+    const apps = await client.get();
+
+    console.log(`Got apps: ${apps}`);
+
+    // Pass data to the page via props
+    return { props: { apps } };
+  } catch (error) {
+    console.log(`Error getting apps: ${error.message}}`);
+    return { props: { apps: [{ appName: 'cat', displayName: 'dog' }] } };
+  }
+};
