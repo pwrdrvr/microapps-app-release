b455818c5239d482dc699b20839e03bf2c185367
Harold <harold@pwrdrvr.com>
2021-02-27 19:43:49 -0500

    Use Deployer Swagger client to get an initial list of Applications

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ApplicationClient } from '../../clients/DeployerClient';
+import fetch from 'node-fetch';
 
-export default (req, res) => {
+export default async function hello(req, res): Promise<void> {
+  const client = new ApplicationClient(undefined, { fetch });
+
   res.statusCode = 200;
-  res.json({ name: 'John Doe' });
-};
+  res.json(await client.get());
+}
