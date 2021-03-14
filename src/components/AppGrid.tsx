851951b8801a7ce52a2178ac1f5f5bb84459878c
Harold <harold@pwrdrvr.com>
2021-03-14 12:18:29 -0400

    Move AppGrid into a component for compartmentalization

---
+++ src/components/AppGrid.tsx
@@ -0,0 +1,185 @@
+import styles from '../styles/AppGrid.module.css';
+import { Grid, AutoSizer } from 'react-virtualized';
+import React from 'react';
+import clsx from 'clsx';
+import { ContentBox } from '../components/ContentBox';
+
+interface IApplication {
+  AppName: string;
+  DisplayName: string;
+}
+
+interface IPageProps {
+  apps: IApplication[];
+}
+
+interface IPageState {
+  columnCount: number;
+  height: number;
+  rowHeight: number;
+  rowCount: number;
+  apps: IApplication[];
+}
+
+export default class AppGrid extends React.PureComponent<IPageProps, IPageState> {
+  constructor(props: IPageProps) {
+    super(props);
+
+    this.state = {
+      columnCount: 3,
+      height: 300,
+      rowHeight: 40,
+      rowCount: this.props.apps.length,
+      apps: this.props.apps,
+    };
+
+    this._cellRenderer = this._cellRenderer.bind(this);
+    this._getRowClassName = this._getRowClassName.bind(this);
+    this._noContentRenderer = this._noContentRenderer.bind(this);
+    this._renderBodyCell = this._renderBodyCell.bind(this);
+    this._renderLeftSideCell = this._renderLeftSideCell.bind(this);
+  }
+
+  render(): JSX.Element {
+    const { columnCount, height, rowHeight, rowCount } = this.state;
+
+    return (
+      <ContentBox className={styles.ContentBox} style={{ marginRight: 5 + 'em' }}>
+        <AutoSizer disableHeight>
+          {({ width }) => (
+            <Grid
+              cellRenderer={this._cellRenderer}
+              className={styles.BodyGrid}
+              columnWidth={this._getColumnWidth}
+              columnCount={columnCount}
+              height={height}
+              noContentRenderer={this._noContentRenderer}
+              overscanColumnCount={0}
+              overscanRowCount={10}
+              rowHeight={rowHeight}
+              rowCount={rowCount}
+              width={width}
+            />
+          )}
+        </AutoSizer>
+      </ContentBox>
+    );
+  }
+
+  _cellRenderer({
+    columnIndex,
+    key,
+    rowIndex,
+    style,
+  }: {
+    columnIndex: number;
+    key: string;
+    rowIndex: number;
+    style: React.CSSProperties;
+  }): JSX.Element {
+    if (columnIndex === 0) {
+      return this._renderLeftSideCell({ key, rowIndex, style });
+    } else {
+      return this._renderBodyCell({ columnIndex, key, rowIndex, style });
+    }
+  }
+
+  _getColumnWidth({ index }: { index: number }): number {
+    switch (index) {
+      case 0:
+        return 50;
+      case 1:
+        return 200;
+      case 2:
+        return 400;
+      default:
+        return 80;
+    }
+  }
+
+  _getDatum(index: number): IApplication {
+    const { apps } = this.state;
+
+    return apps[index];
+  }
+
+  _getRowClassName(row: number): string {
+    return row % 2 === 0 ? styles.evenRow : styles.oddRow;
+  }
+
+  _getRowHeight({ index }: { index: number }): number {
+    //return this._getDatum(index).size;
+    return 40;
+  }
+
+  _noContentRenderer(): JSX.Element {
+    return <div className={styles.noCells}>No cells</div>;
+  }
+
+  _renderBodyCell({
+    columnIndex,
+    key,
+    rowIndex,
+    style,
+  }: {
+    columnIndex: number;
+    key: string;
+    rowIndex: number;
+    style: React.CSSProperties;
+  }): JSX.Element {
+    const rowClass = this._getRowClassName(rowIndex);
+    const datum = this._getDatum(rowIndex);
+
+    let content;
+
+    switch (columnIndex) {
+      case 1:
+        content = datum.AppName;
+        break;
+      case 2:
+        content = datum.DisplayName;
+        break;
+      default:
+        content = `r:${rowIndex}, c:${columnIndex}`;
+        break;
+    }
+
+    const classNames = clsx(rowClass, styles.cell, {
+      [styles.centeredCell]: columnIndex > 2,
+    });
+
+    return (
+      <div className={classNames} key={key} style={style}>
+        {content}
+      </div>
+    );
+  }
+
+  _renderLeftSideCell({
+    key,
+    rowIndex,
+    style,
+  }: {
+    key: string;
+    rowIndex: number;
+    style: React.CSSProperties;
+  }): JSX.Element {
+    const datum = this._getDatum(rowIndex);
+
+    const classNames = clsx(styles.cell, styles.letterCell);
+
+    // Don't modify styles.
+    // These are frozen by React now (as of 16.0.0).
+    // Since Grid caches and re-uses them, they aren't safe to modify.
+    style = {
+      ...style,
+      backgroundColor: 'blue',
+    };
+
+    return (
+      <div className={classNames} key={key} style={style}>
+        {datum.AppName.charAt(0)}
+      </div>
+    );
+  }
+}
